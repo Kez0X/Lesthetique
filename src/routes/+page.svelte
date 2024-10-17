c82bac124ed68ad148bee26a12c97bfa13eec6c8
Kez0X <luka.baudrant@gmail.com>
2024-10-17 10:32:18 +0200

    feat : to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>L'esthétique</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="container">
+	<div class="content">
+		<h1>L'esthétique by Anaïs</h1>
+		<p class="tagline">Beauté à domicile, douceur à chaque instant</p>
+		<p class="description">Massages, épilations, onglerie à domicile pour femmes</p>
+		<button class="construction-btn"><b>En construction ✨</b></button>
+		<p class="follow-us">Nous suivre sur les réseaux sociaux</p>
+		<div class="social-icons">
+			<a href="#"><img src="src/lib/images/Facebook.png" alt="Facebook"></a>
+			<a href="#"><img src="src/lib/images/Instagram.webp" alt="Instagram"></a>
+		</div>
+	</div>
+</div>
+
+<style>
+	
+</style>
