5f0c41ec1596671b22b8ccfd36afd5c2764b1477
Kez0X <118253031+Kez0X@users.noreply.github.com>
2024-10-28 09:58:51 +0000

    feat : new informations in text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,16 @@
 
 <div class="container">
 	<div class="content">
-		<h1>L'esthétique by Anaïs</h1>
+		<h1>L'esthétiqueAA</h1>
 		<p class="tagline">Beauté à domicile, douceur à chaque instant</p>
 		<p class="description">Massages, épilations, onglerie à domicile pour femmes</p>
 		<button class="construction-btn"><b>En construction ✨</b></button>
+		<p class="follow-us">Notre secteur d'activité : Oullins, Pierre Bénite, La Mulatière</p>
+		<button class="construction-btn"><b>Nos prestations 💄</b></button>
 		<p class="follow-us">Nous suivre sur les réseaux sociaux</p>
 		<div class="social-icons">
 			<a href=""><img src={Facebook} alt="Facebook"></a>
-			<a href=""><img src={Instagram} alt="Instagram"></a>
+			<a href="https://www.instagram.com/anaisdelice.estheticienne69?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src={Instagram} alt="Instagram"></a>
 		</div>
 	</div>
 </div>
