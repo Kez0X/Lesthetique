37d4916f93f29851c9e27bbb0da61061d1a7f114
Kez0X <118253031+Kez0X@users.noreply.github.com>
2024-10-29 10:30:02 +0000

    feat : links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 <script>
 	import Instagram from '$lib/images/Instagram.webp';
 	import Facebook from '$lib/images/Facebook.png';
+	import Brochure from '$lib/images/Beige Organic Beauty and Spa Brochure.pdf';
 </script>
 
 <div class="container">
@@ -15,10 +16,10 @@
 		<p class="description">Massages, épilations, onglerie à domicile pour femmes</p>
 		<button class="construction-btn"><b>En construction ✨</b></button>
 		<p class="follow-us">Notre secteur d'activité : Oullins, Pierre Bénite, La Mulatière</p>
-		<button class="construction-btn"><b>Nos prestations 💄</b></button>
+		<button on:click={() => window.location.href = Brochure} class="construction-btn"><b>Nos prestations 💄</b></button>
 		<p class="follow-us">Nous suivre sur les réseaux sociaux</p>
 		<div class="social-icons">
-			<a href=""><img src={Facebook} alt="Facebook"></a>
+			<a href="https://www.facebook.com/share/ckJ3a91v4Mo9PtRF/"><img src={Facebook} alt="Facebook"></a>
 			<a href="https://www.instagram.com/anaisdelice.estheticienne69?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src={Instagram} alt="Instagram"></a>
 		</div>
 	</div>
