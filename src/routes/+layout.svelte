e1ca76d121b722e5ab93496eb21bc87fef82357b
Kez0X <118253031+Kez0X@users.noreply.github.com>
2024-11-05 11:49:23 +0000

    fix : layout use

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,11 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/navbar.svelte';
-	import Prestations from '$lib/prestations.svelte';
-	import About from '$lib/about.svelte';
-	import Payment from '$lib/payment.svelte';
-	import FAQ from '$lib/faq.svelte';
-	import Footer from '$lib/footer.svelte';
-	import Reviews from '$lib/review.svelte';
-	
-	// Import de `page` depuis SvelteKit
-	import { page } from '$app/stores';
-	
-	// Création d’une variable réactive `isContactPage`
-	$: isContactPage = $page.url.pathname === '/contact';
+	import Footer from '$lib/footer.svelte';	
 </script>
 
 <div class="app">
 	<Navbar />
-	
-	<!-- Affiche le contenu principal seulement si ce n’est pas la page de contact -->
-	{#if !isContactPage}
-		<!-- Page d'accueil -->
-		<div class="home-page-container">
-			<div class="page-container">
-				<slot />
-			</div>
-		</div>
-
-		<Prestations />
-		<About />
-		<Payment />
-		<FAQ />
-		<Reviews />
-		<Footer />
-	{/if}
-
-	<!-- Affiche le slot uniquement pour la page de contact -->
-	{#if isContactPage}
-		<div class="contact-page-container">
-			<slot />
-		</div>
-	{/if}
+	<slot />
+	<Footer />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,43 @@
 <script>
 	import Instagram from '$lib/images/Instagram.webp';
 	import Facebook from '$lib/images/Facebook.png';
+	import Prestations from '$lib/prestations.svelte';
+	import About from '$lib/about.svelte';
+	import Payment from '$lib/payment.svelte';
+	import FAQ from '$lib/faq.svelte';
+	import Reviews from '$lib/review.svelte';
+	import { page } from '$app/stores';
 </script>
-	<div class="container">
-		<div class="content">
-			<h1>L'esthétiqueAA</h1>
-			<p class="tagline">Beauté à domicile, douceur à chaque instant</p>
-			<p class="description">Massages, épilations, onglerie à domicile pour femmes</p>
-			<div class="button">
-				<button class="construction-btn" on:click={() => window.location.href = '#prestations'}>
-					<b>Nos prestations ✨</b>
-				</button>
-				<button class="construction-btn" on:click={() => window.location.href = '/contact'}>
-					<b>Prendre rendez-vous 💄</b>
-				</button>
-			</div>
-			<p class="follow-us">Notre secteur d'activité : Oullins, Pierre Bénite, La Mulatière</p>
-			<p class="follow-us">Nous suivre sur les réseaux sociaux</p>
-			<div class="social-icons">
-				<a href=""><img src={Facebook} alt="Facebook"></a>
-				<a href="https://www.instagram.com/anaisdelice.estheticienne69?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src={Instagram} alt="Instagram"></a>
+
+<div class="page-home">
+	<div class="home-page-container">
+		<div class="page-container">
+			<div class="container">
+				<div class="content">
+					<h1>L'esthétiqueAA</h1>
+					<p class="tagline">Beauté à domicile, douceur à chaque instant</p>
+					<p class="description">Massages, épilations, onglerie à domicile pour femmes</p>
+					<div class="button">
+						<button class="construction-btn" on:click={() => window.location.href = '#prestations'}>
+							<b>Nos prestations ✨</b>
+						</button>
+						<button class="construction-btn" on:click={() => window.location.href = '/contact'}>
+							<b>Prendre rendez-vous 💄</b>
+						</button>
+					</div>
+					<p class="follow-us">Notre secteur d'activité : Oullins, Pierre Bénite, La Mulatière</p>
+					<p class="follow-us">Nous suivre sur les réseaux sociaux</p>
+					<div class="social-icons">
+						<a href=""><img src={Facebook} alt="Facebook"></a>
+						<a href="https://www.instagram.com/anaisdelice.estheticienne69?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src={Instagram} alt="Instagram"></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-
-<style>
-	
-</style>
+	<Prestations />
+	<About />
+	<Payment />
+	<FAQ />
+	<Reviews />
+</div>
