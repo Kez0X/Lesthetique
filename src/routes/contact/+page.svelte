98e6fe5208c3b0c7d32d5c140c94cfa5e78d4877
Kez0X <118253031+Kez0X@users.noreply.github.com>
2025-05-08 10:00:34 +0000

    feat : ajout du pixel facebook sur toutes les pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,6 +2,22 @@
     <title>Contact - L'EsthetiqueAA</title>
     <meta name="description" content="Contactez L'esthétique pour prendre rendez-vous et découvrir nos prestations." />
     <meta name="keywords" content="épilation bio, épilation à domicile, soins de beauté bio, épilation Oullins, épilation Saint-Genis-Laval, épilation Lyon, esthétique AA, l'esthétique AA, Esthetique AA, esthetique AA Oullins, esthetique à domicile">
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '669075309377526');
+        fbq('track', 'PageView');
+    </script>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=669075309377526&ev=PageView&noscript=1" alt=""/>
+    <!-- End Meta Pixel Code -->
 </svelte:head>
 
 <script>
